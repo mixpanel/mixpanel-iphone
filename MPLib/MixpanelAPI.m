bd8bffbed54db191348a8a082155c59725b8e4c0
Peter Skirko <peter@mixpanel.com>
2012-01-24 19:00:39 -0800

    Handle when bundle name missing in .plist

--- MPLib/MixpanelAPI.m
+++ MPLib/MixpanelAPI.m
@@ -143,9 +143,17 @@ NSString* calculateHMAC_SHA1(NSString *str, NSString *key) {
     NSDictionary *dict = [self interfaces];
     NSArray *keys = [dict allKeys];
     keys = [keys  sortedArrayUsingSelector:@selector(caseInsensitiveCompare:)];
-    NSString *bundleName = [[[NSBundle mainBundle] infoDictionary] objectForKey:(id)kCFBundleNameKey];
-    NSMutableString *string = [NSMutableString stringWithString:bundleName];
-    for (NSString *key in keys) {
+ 
+	NSString *bundleName = [[[NSBundle mainBundle] infoDictionary] objectForKey:(id)kCFBundleNameKey];
+ 
+	/* while most apps will define CFBundleName, it's not guaranteed -- an app can choose to define it or not
+	   so when it's missing, use the bundle file name */	
+	if (bundleName == nil) {
+		bundleName = [[[NSBundle mainBundle] bundlePath] lastPathComponent];
+	}
+
+	NSMutableString *string = [NSMutableString stringWithString:bundleName];	
+	for (NSString *key in keys) {
         [string appendString:[dict objectForKey:key]];
     }
     return string;
