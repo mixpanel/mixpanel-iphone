2017090d4d01771245e78019a0c48e98d4282afb
Kasra Kyanzadeh <kasra@mixpanel.com>
2012-06-28 21:08:36 -0700

    Removed test code

--- MixpanelEventSample/Classes/MixpanelEventSampleAppDelegate.m
+++ MixpanelEventSample/Classes/MixpanelEventSampleAppDelegate.m
@@ -7,7 +7,7 @@
 #import "MixpanelEventSampleAppDelegate.h"
 #import "MixpanelEventSampleViewController.h"
 #import "MixpanelAPI.h"
-#define MIXPANEL_TOKEN @"886377995387084ca48b4c7a1d6e1aa3"
+#define MIXPANEL_TOKEN @"YOUR TOKEN HERE"
 @implementation MixpanelEventSampleAppDelegate
 
 @synthesize window;
@@ -23,11 +23,10 @@
     //Initialize the MixpanelAPI object
     mixpanel = [MixpanelAPI sharedAPIWithToken:MIXPANEL_TOKEN];
     
-    // Set the upload interval to 5 seconds for testings
+    // Set the upload interval to 5 seconds for testing
     // If not set, it defaults to 30 seconds
     [mixpanel setUploadInterval:5];
-    //[mixpanel setServerURL:@"http://m.kasrak.com"];
-
+    
     // Add the view controller's view to the window and display.
     [window addSubview:viewController.view];
     [window makeKeyAndVisible];
--- MixpanelEventSample/Unit Tests/Unit_Tests.h
+++ MixpanelEventSample/Unit Tests/Unit_Tests.h
@@ -10,7 +10,7 @@
 #import "MPCJSONSerializer.h"
 
 #define MP_TEST_UPLOAD_INTERVAL 3
-#define MP_TEST_TOKEN @"886377995387084ca48b4c7a1d6e1aa3"
+#define MP_TEST_TOKEN @"test token"
 
 @interface Unit_Tests : SenTestCase {
     MixpanelAPI *mp;
