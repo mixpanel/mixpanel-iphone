4881836479052b2d8a8bef438d5c14936b43c66c
Neil Rahilly <neil@mixpanel.com>
2012-11-12 14:33:09 -0800

    add required imports

--- Mixpanel/Mixpanel.h
+++ Mixpanel/Mixpanel.h
@@ -16,6 +16,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
 @class    MixpanelPeople;
 @protocol MixpanelDelegate;
 
