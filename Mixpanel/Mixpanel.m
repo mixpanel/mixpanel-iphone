6567d6375c0cdd03994339446ca3606734c4a1da
Steve McKenzie <steve@stevemckenzie.ca>
2013-02-19 00:18:55 -0800

    Updated the other instance of collecting the app version.

--- Mixpanel/Mixpanel.m
+++ Mixpanel/Mixpanel.m
@@ -1049,7 +1049,7 @@ static Mixpanel *sharedInstance = nil;
     NSMutableDictionary *properties = [NSMutableDictionary dictionary];
     [properties setValue:[Mixpanel deviceModel] forKey:@"$ios_device_model"];
     [properties setValue:[device systemVersion] forKey:@"$ios_version"];
-    [properties setValue:[[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleVersion"] forKey:@"$ios_app_version"];
+    [properties setValue:[[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleShortVersionString"] forKey:@"$ios_app_version"];
     return [NSDictionary dictionaryWithDictionary:properties];
 }
 
