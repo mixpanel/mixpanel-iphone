42a5998d25dff2309b8c9c894a91bcdf598f3682
Patrick Piemonte <patrick@diy.org>
2013-07-23 16:03:00 -0700

    ODIN: fixes a potential memory leak

--- Mixpanel/Library/ODIN/ODIN.m
+++ Mixpanel/Library/ODIN/ODIN.m
@@ -58,6 +58,8 @@ NSString * ODIN1(){
     
     if (sysctl(mib, 6, buf, &len, NULL, 0) < 0) {
         //NSLog(@"ODIN-1.1: sysctl 2 error");
+        if (buf)
+            free(buf);
         return nil;
     }
     
