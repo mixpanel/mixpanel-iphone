e6a5a0209ceb4d0628fe41754e462b6fe50f7bf2
Neil Rahilly <neil@mixpanel.com>
2013-09-05 16:18:41 -0700

    use NSInvocation for drawViewHierarchyInRect to get rid of compiler warning

--- Mixpanel/UIView+MPSnapshotImage.m
+++ Mixpanel/UIView+MPSnapshotImage.m
@@ -4,11 +4,23 @@
 
 @implementation UIView (MPSnapshotImage)
 
+// taken from https://github.com/romaonthego/RESideMenu/blob/master/RESideMenu/UIView%2BImageSnapshot.m
 - (UIImage *)mp_snapshotImage
 {
-    UIGraphicsBeginImageContextWithOptions(self.bounds.size, NO, [UIScreen mainScreen].scale);
+    if ([[UIScreen mainScreen] respondsToSelector:@selector(scale)]) {
+        UIGraphicsBeginImageContextWithOptions(self.bounds.size, NO, [UIScreen mainScreen].scale);
+    } else {
+        UIGraphicsBeginImageContext(self.bounds.size);
+    }
     if ([self respondsToSelector:@selector(drawViewHierarchyInRect:)]) {
-        [self drawViewHierarchyInRect:self.bounds];
+        NSInvocation* invoc = [NSInvocation invocationWithMethodSignature:
+                               [self methodSignatureForSelector:
+                                @selector(drawViewHierarchyInRect:)]];
+        [invoc setTarget:self];
+        [invoc setSelector:@selector(drawViewHierarchyInRect:)];
+        CGRect arg2 = self.bounds;
+        [invoc setArgument:&arg2 atIndex:2];
+        [invoc invoke];
     } else {
         [self.layer renderInContext:UIGraphicsGetCurrentContext()];
     }
