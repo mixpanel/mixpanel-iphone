ba15f8aac0baf59a77cf79b4c06935b7c84878cf
Kyle Warren <qarren@gmail.com>
2013-10-18 17:47:49 -0700

    started adding notif ui elements

---
+++ Mixpanel/MPNotificationViewController.h
@@ -0,0 +1,13 @@
+//
+//  MPNotificationViewController.h
+//  HelloMixpanel
+//
+//  Created by Kyle Warren on 10/18/13.
+//  Copyright (c) 2013 Mixpanel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MPNotificationViewController : UIViewController
+
+@end
---
+++ Mixpanel/MPNotificationViewController.m
@@ -0,0 +1,38 @@
+//
+//  MPNotificationViewController.m
+//  HelloMixpanel
+//
+//  Created by Kyle Warren on 10/18/13.
+//  Copyright (c) 2013 Mixpanel. All rights reserved.
+//
+
+#import "MPNotificationViewController.h"
+
+@interface MPNotificationViewController ()
+
+@end
+
+@implementation MPNotificationViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
