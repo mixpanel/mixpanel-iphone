9cf955399b5b7d74f0517640b475cd88e7db7f15
alex-hofsteede <alexh@mixpanel.com>
2013-10-24 09:30:42 -0700

    Remove autogenerated pods stuff. use Analyzing dependencies [32m CocoaPods 0.27.1 is available. [0m Downloading dependencies Using CocoaAsyncSocket (0.0.1) Using CocoaHTTPServer (2.2.1) Using CocoaLumberjack (1.3.3) Generating Pods project Integrating client project to install

--- HelloMixpanel/Pods/Pods-HelloMixpanelTests-CocoaAsyncSocket-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_HelloMixpanelTests_CocoaAsyncSocket : NSObject
-@end
-@implementation PodsDummy_Pods_HelloMixpanelTests_CocoaAsyncSocket
-@end
--- HelloMixpanel/Pods/Pods-HelloMixpanelTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_HelloMixpanelTests : NSObject
-@end
-@implementation PodsDummy_Pods_HelloMixpanelTests
-@end
