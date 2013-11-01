94b61c4aa4b51a82b6af13bb34db8b8173ceab33
Kyle Warren <qarren@gmail.com>
2013-10-31 17:58:18 -0700

    added a gradient layer

--- Mixpanel/MPNotificationViewController.m
+++ Mixpanel/MPNotificationViewController.m
@@ -9,6 +9,11 @@
 #import "MPNotificationViewController.h"
 
 #import "MPNotification.h"
+#import "UIImage+MPAverageColor.h"
+#import "UIImage+MPImageEffects.h"
+#import "UIView+MPSnapshotImage.h"
+
+#import <QuartzCore/QuartzCore.h>
 
 @interface MPNotificationViewController () {
     id _target;
@@ -19,6 +24,8 @@
 @property (nonatomic, retain) IBOutlet UILabel *titleView;
 @property (nonatomic, retain) IBOutlet UILabel *bodyView;
 @property (nonatomic, retain) IBOutlet UIButton *okayButton;
+@property (nonatomic, retain) IBOutlet UIView *bodyBg;
+@property (nonatomic, retain) IBOutlet UIImageView *backgroundImageView;
 
 @end
 
@@ -36,6 +43,7 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    self.backgroundImageView.image = [_backgroundImage mp_applyDarkEffect];
     
     if (self.notification) {
         NSLog(@"notif: %@", self.notification.body);
@@ -49,6 +57,33 @@
         [self.okayButton setTitle:self.notification.cta forState:UIControlStateNormal];
         [self.okayButton sizeToFit];
     }
+    
+    self.okayButton.layer.cornerRadius = 15.0f;
+    self.okayButton.layer.borderColor = [UIColor whiteColor].CGColor;
+    self.okayButton.layer.borderWidth = 2.0f;
+    
+//    self.bodyBg.layer.shadowOffset = CGSizeMake(0.0f, -20.0f);
+//    self.bodyBg.layer.shadowOpacity = 1.0f;
+//    self.bodyBg.layer.shadowRadius = 5.0f;
+//    self.bodyBg.layer.shadowColor = [UIColor blackColor].CGColor;
+    
+    CAGradientLayer *fadeLayer = [CAGradientLayer layer];
+    CGColorRef outerColor = [UIColor colorWithWhite:1 alpha:0].CGColor;
+    CGColorRef innerColor = [UIColor colorWithWhite:1 alpha:1].CGColor;
+    fadeLayer.colors = @[(id)outerColor, (id)innerColor, (id)innerColor];
+    // add 20 pixels of fade in and out at top and bottom of table view container
+    CGFloat offset = 44 / self.bodyBg.bounds.size.height;
+    fadeLayer.locations = @[@0, @(0 + offset), @(1 - offset), @1];
+    fadeLayer.bounds = self.bodyBg.bounds;
+    fadeLayer.anchorPoint = CGPointZero;
+    self.bodyBg.layer.mask = fadeLayer;
+    
+    self.imageView.contentMode = UIViewContentModeScaleAspectFill;
+    self.imageView.layer.shadowOffset = CGSizeMake(0.0f, 0.0f);
+    self.imageView.layer.shadowOpacity = 1.0f;
+    self.imageView.layer.shadowRadius = 5.0f;
+    self.imageView.layer.shadowColor = [UIColor blackColor].CGColor;
+    //self.bodyBg.backgroundColor = [UIColor blackColor];
     
     [self.okayButton addTarget:self action:@selector(pressedOkay) forControlEvents:UIControlEventTouchDown];
 }
