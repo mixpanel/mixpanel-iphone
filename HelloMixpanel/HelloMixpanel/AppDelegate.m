01119a7992a954416ddcb451d936724388d5e458
Kyle Warren <qarren@gmail.com>
2013-11-20 14:02:16 -0800

    default notif type to takeover

--- HelloMixpanel/HelloMixpanel/AppDelegate.m
+++ HelloMixpanel/HelloMixpanel/AppDelegate.m
@@ -49,6 +49,7 @@
     
     self.mixpanel.checkForNotificationsOnActive = YES;
     self.mixpanel.showNotificationOnActive = NO; //Change this to NO to show your notifs manually.
+    self.mixpanel.showNotificationType = @"takeover";
 
     // Set the upload interval to 20 seconds for demonstration purposes. This would be overkill for most applications.
     self.mixpanel.flushInterval = 20; // defaults to 60 seconds
