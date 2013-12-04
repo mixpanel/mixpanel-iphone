1466fc8132874d026a7dddbc1bc96d1c49ce125d
alex-hofsteede <alexh@mixpanel.com>
2013-12-04 15:43:57 -0800

    Restore default settings

--- HelloMixpanel/HelloMixpanel/AppDelegate.m
+++ HelloMixpanel/HelloMixpanel/AppDelegate.m
@@ -23,7 +23,7 @@
 #import "ViewController.h"
 
 // IMPORTANT!!! replace with you api token from https://mixpanel.com/account/
-#define MIXPANEL_TOKEN @"metrics-1"
+#define MIXPANEL_TOKEN @"YOUR_MIXPANEL_PROJECT_TOKEN"
 
 @implementation AppDelegate
 
