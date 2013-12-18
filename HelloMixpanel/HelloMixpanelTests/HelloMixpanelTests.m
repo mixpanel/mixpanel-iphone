d04ebb248a5c330e62c07e14f16784c29750854c
alex-hofsteede <alexh@mixpanel.com>
2013-12-18 15:08:39 -0800

    Fix crash caused by trying to present a survey on top of a viewcontroller that was itself being presented

--- HelloMixpanel/HelloMixpanelTests/HelloMixpanelTests.m
+++ HelloMixpanel/HelloMixpanelTests/HelloMixpanelTests.m
@@ -23,6 +23,7 @@
 #import "MPSurveyQuestion.h"
 #import "HTTPServer.h"
 #import "MixpanelDummyHTTPConnection.h"
+#import "MPSurveyNavigationController.h"
 
 #define TEST_TOKEN @"abc123"
 
@@ -141,6 +142,14 @@
     STAssertNotNil(p[@"$ios_app_version"], @"missing $ios_app_version property");
     STAssertNotNil(p[@"$ios_app_release"], @"missing $ios_app_release property");
     STAssertNotNil(p[@"$ios_ifa"], @"missing $ios_ifa property");
+}
+
+-(UIViewController *)topViewController {
+    UIViewController *rootViewController = [UIApplication sharedApplication].keyWindow.rootViewController;
+    while (rootViewController.presentedViewController) {
+        rootViewController = rootViewController.presentedViewController;
+    }
+    return rootViewController;
 }
 
 - (void)testHTTPServer
@@ -914,6 +923,36 @@
     STAssertNil([MPSurveyQuestion questionWithJSONObject:m], nil);
 }
 
+- (void)testNoShowSurveyOnPresentingVC
+{
+    NSDictionary *o = @{@"id": @3,
+                        @"name": @"survey",
+                        @"collections": @[@{@"id": @9, @"name": @"collection"}],
+                        @"questions": @[@{
+                                            @"id": @12,
+                                            @"type": @"text",
+                                            @"prompt": @"Anything else?",
+                                            @"extra_data": @{}}]};
+
+    MPSurvey *survey = [MPSurvey surveyWithJSONObject:o];
+
+    //Start presenting a View Controller on the current root
+    UIViewController *topViewController = [self topViewController];
+
+    __block BOOL waitForBlock = YES;
+    [topViewController presentViewController:[[UIViewController alloc]init] animated:YES completion:^{ waitForBlock = NO; }];
+
+    //Survey should not show as it cannot present on top of a currently presenting view controller
+    [self.mixpanel performSelector:@selector(presentSurveyWithRootViewController:) withObject:survey];
+
+    STAssertFalse([[self topViewController] isKindOfClass:[MPSurveyNavigationController class]], @"Survey was presented when it shouldn't have been");
+
+    //Wait for original VC to present, so we don't interfere with subsequent tests.
+    while(waitForBlock) {
+        [[NSRunLoop currentRunLoop] runMode:NSDefaultRunLoopMode beforeDate:[NSDate dateWithTimeIntervalSinceNow:0.1]];
+    }
+}
+
 - (void)testShowSurvey
 {
     NSDictionary *o = @{@"id": @3,
@@ -928,7 +967,8 @@
     MPSurvey *survey = [MPSurvey surveyWithJSONObject:o];
 
     [self.mixpanel performSelector:@selector(presentSurveyWithRootViewController:) withObject:survey];
-    // No assertions, test pass = no crash
+
+    STAssertTrue([[self topViewController] isKindOfClass:[MPSurveyNavigationController class]], @"Survey was not presented");
 }
 
 
--- Mixpanel/Mixpanel.m
+++ Mixpanel/Mixpanel.m
@@ -403,7 +403,7 @@ static Mixpanel *sharedInstance = nil;
         distinctId = [[ASIdentifierManager sharedManager].advertisingIdentifier UUIDString];
     }
 #endif
-    
+
     if (!distinctId) {
         NSLog(@"%@ error getting device identifier: falling back to uuid", self);
         distinctId = [[NSUUID UUID] UUIDString];
@@ -927,6 +927,18 @@ static Mixpanel *sharedInstance = nil;
 
 #pragma mark - Surveys
 
++ (UIViewController *)topViewController
+{
+    UIViewController *rootViewController = [UIApplication sharedApplication].keyWindow.rootViewController;
+    while (rootViewController.presentedViewController) {
+        rootViewController = rootViewController.presentedViewController;
+    }
+    if (rootViewController == nil || [rootViewController isBeingPresented] || [rootViewController isBeingDismissed]) {
+        return nil;
+    }
+    return rootViewController;
+}
+
 - (void)checkForSurveysWithCompletion:(void (^)(NSArray *surveys))completion
 {
     dispatch_async(self.serialQueue, ^{
@@ -995,16 +1007,15 @@ static Mixpanel *sharedInstance = nil;
 
 - (void)presentSurveyWithRootViewController:(MPSurvey *)survey
 {
-    UIViewController *rootViewController = [UIApplication sharedApplication].keyWindow.rootViewController;
-    while (rootViewController.presentedViewController) {
-        rootViewController = rootViewController.presentedViewController;
+    UIViewController *rootViewController = [Mixpanel topViewController];
+    if (rootViewController) {
+        UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"MPSurvey" bundle:nil];
+        MPSurveyNavigationController *controller = [storyboard instantiateViewControllerWithIdentifier:@"MPSurveyNavigationController"];
+        controller.survey = survey;
+        controller.delegate = self;
+        controller.backgroundImage = [rootViewController.view mp_snapshotImage];
+        [rootViewController presentViewController:controller animated:YES completion:nil];
     }
-    UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"MPSurvey" bundle:nil];
-    MPSurveyNavigationController *controller = [storyboard instantiateViewControllerWithIdentifier:@"MPSurveyNavigationController"];
-    controller.survey = survey;
-    controller.delegate = self;
-    controller.backgroundImage = [rootViewController.view mp_snapshotImage];
-    [rootViewController presentViewController:controller animated:YES completion:nil];
 }
 
 - (void)showSurveyWithObject:(MPSurvey *)survey withAlert:(BOOL)showAlert
