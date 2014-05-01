c2f4af2e84ee814acc0cacb8732e073f132f39f3
alex-hofsteede <alexh@mixpanel.com>
2014-05-01 14:52:03 -0700

    WIP Add MPSwizzler

---
+++ Mixpanel/MPSwizzler.h
@@ -0,0 +1,16 @@
+//
+//  MPSwizzler.h
+//  HelloMixpanel
+//
+//  Created by Alex Hofsteede on 1/5/14.
+//  Copyright (c) 2014 Mixpanel. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MPSwizzler : NSObject
+
++ (void)swizzleSelector:(SEL)selector onClass:(Class)class;
++ (void)unswizzleSelector:(SEL)selector onClass:(Class)class;
+
+@end
