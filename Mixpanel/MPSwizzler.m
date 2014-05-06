f38a9436e493ab0cbd84c6d61dc2b944a03864cf
alex-hofsteede <alexh@mixpanel.com>
2014-05-06 16:11:02 -0700

    Test swizzling code to aplly AB test variant to view when it appears. Warning. This is still a hack

--- Mixpanel/MPSwizzler.m
+++ Mixpanel/MPSwizzler.m
@@ -10,25 +10,28 @@
 #import "MPSwizzler.h"
 
 #pragma message("TODO: We need one of these for every pair of class/selector we want to swizzle")
-static IMP __original_Method_Imp;
+static IMP _originalMethod;
+static void (^_block)();
 
-void mp_swizzledMethod(id self, SEL _cmd, UIWindow *window)
+void mp_swizzledMethod(id self, SEL _cmd, UIWindow *window )
 {
+    NSLog(@"Running swizzled method");
     assert([NSStringFromSelector(_cmd) isEqualToString:@"willMoveToWindow:"]);
 
-    NSLog(@"I'm in ur view!");
-    // Check for outstanding AB tests for this view here.
+    ((void(*)(id, SEL, UIWindow*))_originalMethod)(self, _cmd, window);
 
-    ((void(*)(id, SEL, UIWindow*))__original_Method_Imp)(self, _cmd, window);
+    _block();
+
 }
 
 @implementation MPSwizzler
 
-+ (void)swizzleSelector:(SEL)selector onClass:(Class)class
++ (void)swizzleSelector:(SEL)selector onClass:(Class)class withBlock:(void (^)())block
 {
-    if (!__original_Method_Imp) {
+    if (!_originalMethod) {
         Method m = class_getInstanceMethod(class, selector);
-        __original_Method_Imp = method_getImplementation(m);
+        _originalMethod = method_getImplementation(m);
+        _block = block;
 
         // In the case where we got the Method from the superclass, make sure we actually replace it
         // on this class rather than the superclass.
@@ -40,10 +43,11 @@ void mp_swizzledMethod(id self, SEL _cmd, UIWindow *window)
 
 + (void)unswizzleSelector:(SEL)selector onClass:(Class)class
 {
-    if (__original_Method_Imp) {
+    if (_originalMethod) {
         Method m = class_getInstanceMethod(class, selector);
-        __original_Method_Imp = method_setImplementation(m, __original_Method_Imp);
-        __original_Method_Imp = nil;
+        _originalMethod = method_setImplementation(m, _originalMethod);
+        _originalMethod = nil;
+        _block = nil;
     }
 }
 
