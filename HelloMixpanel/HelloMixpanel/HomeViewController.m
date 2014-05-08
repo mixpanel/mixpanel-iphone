06ab6098a00277859e987b45de40b1d9054d2ae3
alex-hofsteede <alexh@mixpanel.com>
2014-05-07 18:31:25 -0700

    Respond to tweak message

--- HelloMixpanel/HelloMixpanel/HomeViewController.m
+++ HelloMixpanel/HelloMixpanel/HomeViewController.m
@@ -13,7 +13,7 @@
 
     self.navigationItem.titleView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"MPLogo.png"]];
 
-    FBTweakBind(self.view, alpha, @"Main Window", @"View", @"Alpha", 1.0f);
+    FBTweakBind(self.view, alpha, @"Main Window", @"View", @"Alpha", 1.0f, 0.0f, 1.0f);
 }
 
 @end
