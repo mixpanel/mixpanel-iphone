3f7090f1f035348dcdd97686a5848c84f17d75a7
alex-hofsteede <alexh@mixpanel.com>
2014-05-09 09:45:52 -0700

    Only swizzle methods that take a single object argument, variable arguments is a huge minefield

--- Mixpanel/MPSwizzler.h
+++ Mixpanel/MPSwizzler.h
@@ -10,7 +10,7 @@
 
 @interface MPSwizzler : NSObject
 
-+ (void)swizzleSelector:(SEL)selector onClass:(Class)class withBlock:(void (^)(void))block;
++ (void)swizzleSelector:(SEL)selector onClass:(Class)class withBlock:(void (^)(id))block;
 + (void)unswizzleSelector:(SEL)selector onClass:(Class)class;
 
 @end
