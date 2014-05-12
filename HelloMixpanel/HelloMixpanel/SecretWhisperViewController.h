87af9576d2fa7c55b9c2a51418dd617d677cf4bf
alex-hofsteede <alexh@mixpanel.com>
2014-05-12 15:30:28 -0700

    Secret Whisper, the mixpanel anonymous gossip app

---
+++ HelloMixpanel/HelloMixpanel/SecretWhisperViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecretWhisperViewControllerTableViewController.h
+//  HelloMixpanel
+//
+//  Created by Alex Hofsteede on 12/5/14.
+//  Copyright (c) 2014 Mixpanel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecretWhisperViewController : UIViewController <UITableViewDataSource, UITableViewDelegate>
+
+@end
