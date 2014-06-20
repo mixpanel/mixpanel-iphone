92aae6cd052b27db4ba7ba0b29fce72c004610ce
Kyle Warren <qarren@gmail.com>
2014-06-20 13:24:16 -0700

    updated version number and docs

--- Mixpanel/Mixpanel.m
+++ Mixpanel/Mixpanel.m
@@ -21,7 +21,7 @@
 #import "NSData+MPBase64.h"
 #import "UIView+MPSnapshotImage.h"
 
-#define VERSION @"2.3.6"
+#define VERSION @"2.4.0"
 
 #ifdef MIXPANEL_LOG
 #define MixpanelLog(...) NSLog(__VA_ARGS__)
