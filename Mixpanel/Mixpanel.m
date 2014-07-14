12b03abea61f721b99e5b377127a3a1decd8b442
alex-hofsteede <alexh@mixpanel.com>
2014-07-14 16:33:21 -0700

    Version 2.4.1

--- Mixpanel/Mixpanel.m
+++ Mixpanel/Mixpanel.m
@@ -21,7 +21,7 @@
 #import "NSData+MPBase64.h"
 #import "UIView+MPSnapshotImage.h"
 
-#define VERSION @"2.4.0"
+#define VERSION @"2.4.1"
 
 #ifdef MIXPANEL_LOG
 #define MixpanelLog(...) NSLog(__VA_ARGS__)
