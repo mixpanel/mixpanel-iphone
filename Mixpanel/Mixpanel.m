0c4700f4d3d81696a438fd3603b5c3346da27156
alex-hofsteede <alexh@mixpanel.com>
2014-07-14 16:25:27 -0700

    Setting the calendar manually isn't required, the en_US_POSIX locale is enough

--- Mixpanel/Mixpanel.m
+++ Mixpanel/Mixpanel.m
@@ -150,8 +150,6 @@ static Mixpanel *sharedInstance = nil;
         self.dateFormatter = [[NSDateFormatter alloc] init];
         [_dateFormatter setDateFormat:@"yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"];
         [_dateFormatter setTimeZone:[NSTimeZone timeZoneWithAbbreviation:@"UTC"]];
-        NSCalendar *calendar = [[NSCalendar alloc] initWithCalendarIdentifier:NSGregorianCalendar];
-        [_dateFormatter setCalendar:calendar];
         [_dateFormatter setLocale:[[NSLocale alloc] initWithLocaleIdentifier:@"en_US_POSIX"]];
 
         self.showSurveyOnActive = YES;
