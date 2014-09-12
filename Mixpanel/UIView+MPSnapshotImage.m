f359a137cc6682f50189b93cec3445e762ee3e0a
Kyle Warren <qarren@gmail.com>
2014-09-12 15:56:02 -0700

    drawViewHierarchyInRect:afterScreenUpdates: draws akward sizes on iphone6. remove it for now.

--- Mixpanel/UIView+MPSnapshotImage.m
+++ Mixpanel/UIView+MPSnapshotImage.m
@@ -23,15 +23,7 @@
     CGContextRef context = UIGraphicsGetCurrentContext();
     CGContextTranslateCTM(context, 0.0f, -offsetHeight);
 
-#if __IPHONE_OS_VERSION_MAX_ALLOWED >= 70000
-    if ([self respondsToSelector:@selector(drawViewHierarchyInRect:afterScreenUpdates:)]) {
-        [self drawViewHierarchyInRect:CGRectMake(0.0f, 0.0f, size.width, size.height) afterScreenUpdates:YES];
-    } else {
-        [self.layer renderInContext:UIGraphicsGetCurrentContext()];
-    }
-#else
     [self.layer renderInContext:UIGraphicsGetCurrentContext()];
-#endif
 
     UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
     UIGraphicsEndImageContext();
