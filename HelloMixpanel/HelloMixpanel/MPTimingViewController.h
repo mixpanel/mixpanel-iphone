a0cd29032e33cbb20a5b0f1d508c308a77d211e2
Kyle Warren <qarren@gmail.com>
2014-09-15 10:40:31 -0700

    added page for testing timing

---
+++ HelloMixpanel/HelloMixpanel/MPTimingViewController.h
@@ -0,0 +1,13 @@
+//
+//  MPTimingViewController.h
+//  HelloMixpanel
+//
+//  Created by Kyle Warren on 9/12/14.
+//  Copyright (c) 2014 Mixpanel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MPTimingViewController : UIViewController
+
+@end
