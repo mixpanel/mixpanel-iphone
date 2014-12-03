5d41756318e845b1006deef1a60872136a99fa04
alex-hofsteede <alexh@mixpanel.com>
2014-12-02 16:38:42 -0800

    bump version

--- Mixpanel/Mixpanel.m
+++ Mixpanel/Mixpanel.m
@@ -28,7 +28,7 @@
 #import "MPWebSocket.h"
 #import "NSData+MPBase64.h"
 
-#define VERSION @"2.6.0"
+#define VERSION @"2.6.1"
 
 @interface Mixpanel () <UIAlertViewDelegate, MPSurveyNavigationControllerDelegate, MPNotificationViewControllerDelegate> {
     NSUInteger _flushInterval;
