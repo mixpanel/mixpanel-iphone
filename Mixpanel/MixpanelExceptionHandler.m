8d5665e9e7160d2b6f1d308cca4644e394f67477
Sam Green <greensamuelm@gmail.com>
2015-07-28 15:47:15 -0700

    Added MixpanelExceptionHandler.

---
+++ Mixpanel/MixpanelExceptionHandler.m
@@ -0,0 +1,70 @@
+//
+//  MixpanelExceptionHandler.m
+//  HelloMixpanel
+//
+//  Created by Sam Green on 7/28/15.
+//  Copyright (c) 2015 Mixpanel. All rights reserved.
+//
+
+#import "MixpanelExceptionHandler.h"
+#import "Mixpanel.h"
+#import "MPLogger.h"
+
+@interface MixpanelExceptionHandler ()
+
+@property (nonatomic) NSUncaughtExceptionHandler *defaultExceptionHandler;
+@property (nonatomic, strong) NSHashTable *mixpanelInstances;
+
+@end
+
+@implementation MixpanelExceptionHandler
+
++ (instancetype)sharedHandler {
+    static MixpanelExceptionHandler *gSharedHandler = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        gSharedHandler = [[MixpanelExceptionHandler alloc] init];
+    });
+    return gSharedHandler;
+}
+
+- (instancetype)init {
+    self = [super init];
+    if (self) {
+        // Create a hash table of weak pointers to mixpanel instances
+        _mixpanelInstances = [NSHashTable weakObjectsHashTable];
+        
+        // Save the existing exception handler
+        _defaultExceptionHandler = NSGetUncaughtExceptionHandler();
+        // Install our handler
+        NSSetUncaughtExceptionHandler(&mp_handleUncaughtException);
+    }
+    return self;
+}
+
+- (void)addMixpanelInstance:(Mixpanel *)instance {
+    NSParameterAssert(instance != nil);
+    
+    [self.mixpanelInstances addObject:instance];
+}
+
+static void mp_handleUncaughtException(NSException *exception) {
+    MixpanelExceptionHandler *handler = [MixpanelExceptionHandler sharedHandler];
+    
+    // Archive the values for each Mixpanel instance
+    for (Mixpanel *instance in handler.mixpanelInstances) {
+        // Since we're storing the instances in a weak table, we need to ensure the pointer hasn't become nil
+        if (instance) {
+            [instance archive];
+        }
+    }
+    
+    MixpanelError(@"Encountered an uncaught exception. All Mixpanel instances were archived.");
+    
+    if (handler.defaultExceptionHandler) {
+        // Ensure the existing handler gets called once we're finished
+        handler.defaultExceptionHandler(exception);
+    }
+}
+
+@end
