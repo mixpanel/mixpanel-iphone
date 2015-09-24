e81583de9c674934f679cce98fcdb991a4e8496a
John Langley <john.langley@omadahealth.com>
2015-09-23 17:01:39 -0700

    Fixing issue from merge where methods were defined twice

--- Mixpanel/Mixpanel.h
+++ Mixpanel/Mixpanel.h
@@ -535,45 +535,6 @@
  @method
 
  @abstract
- Starts a timer that will be stopped and added as a property when a
- corresponding event is tracked.
-
- @discussion
- This method is intended to be used in advance of events that have
- a duration. For example, if a developer were to track an "Image Upload" event
- she might want to also know how long the upload took. Calling this method
- before the upload code would implicitly cause the <code>track</code>
- call to record its duration.
-
- <pre>
- // begin timing the image upload
- [mixpanel timeEvent:@"Image Upload"];
-
- // upload the image
- [self uploadImageWithSuccessHandler:^{
-
-    // track the event
-    [mixpanel track:@"Image Upload"];
- }];
- </pre>
-
- @param event   a string, identical to the name of the event that will be tracked
-
- */
-- (void)timeEvent:(NSString *)event;
-
-/*!
- @method
-
- @abstract
- Clears all current event timers.
- */
-- (void)clearTimedEvents;
-
-/*!
- @method
-
- @abstract
  Clears all stored properties and distinct IDs. Useful if your app's user logs out.
  */
 - (void)reset;
