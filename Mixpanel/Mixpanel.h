e49def3797966427b9f6cf97ed31b3e69e5d1cda
Sohail Ahmed <sohail@appstronomy.com>
2015-10-25 23:03:09 -0400

    Hooked transmitIPAddress property into -flushQueue:endpoint: method.
    When building the postBody, we now respect the client app's property level setting to potentially suppress transmission of the user's IP address.

--- Mixpanel/Mixpanel.h
+++ Mixpanel/Mixpanel.h
@@ -191,10 +191,11 @@
  @property
  
  @abstract
- Controls whether to automatically send the device's IP Address to
- Mixpanel. Transmission allows Mixpanel to geo-locate a user down to
- city-level specification. For privacy reasons, you may need to forego
- having such collection.
+ Controls whether to automatically send the client IP Address as part of 
+ event tracking. With an IP address, geo-location is possible down to neighborhoods
+ with a city, although the Mixpanel Dashboard will just show you city level location
+ specificity. For privacy reasons, you may be in a situation where you need to forego
+ effectively having access to such granular location information via the IP Address.
  
  @discussion
  Defaults to YES.
