090cb6baf100bab76c31f64611dbf4f70b308faf
Suen <lovesunstar@sina.com>
2015-12-27 13:44:35 +0800

    Add class method to get libVersion

--- Mixpanel/Mixpanel.m
+++ Mixpanel/Mixpanel.m
@@ -942,8 +942,14 @@ static __unused NSString *MPURLEncode(NSString *s)
 
 - (NSString *)libVersion
 {
+    return [Mixpanel libVersion];
+}
+
++ (NSString *)libVersion
+{
     return VERSION;
 }
+
 
 - (NSDictionary *)collectAutomaticProperties
 {
