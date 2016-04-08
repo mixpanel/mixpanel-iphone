50ad394a822d40ce521c97c4e1aa7d651c11df95
samgreen <greensamuelm@gmail.com>
2016-04-08 09:46:47 -0700

    Update to mixpanel project.

--- Mixpanel/MPSwizzle.h
+++ Mixpanel/MPSwizzle.h
@@ -8,7 +8,13 @@
 
 #import <Foundation/Foundation.h>
 
+NS_ASSUME_NONNULL_BEGIN
+
 @interface NSObject (MPSwizzle)
+
 + (BOOL)mp_swizzleMethod:(SEL)origSel_ withMethod:(SEL)altSel_ error:(NSError **)error_;
 + (BOOL)mp_swizzleClassMethod:(SEL)origSel_ withClassMethod:(SEL)altSel_ error:(NSError **)error_;
+
 @end
+
+NS_ASSUME_NONNULL_END
