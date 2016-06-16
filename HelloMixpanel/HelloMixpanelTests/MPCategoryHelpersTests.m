960def2e66b6ca8961f341e3f1108789aae91198
samgreen <greensamuelm@gmail.com>
2016-06-15 21:50:36 -0700

    Clean up category helper tests.

--- HelloMixpanel/HelloMixpanelTests/MPCategoryHelpersTests.m
+++ HelloMixpanel/HelloMixpanelTests/MPCategoryHelpersTests.m
@@ -10,6 +10,20 @@
 #import <UIKit/UIKit.h>
 #import <XCTest/XCTest.h>
 
+@interface UIView (MPCategoryHelpersTests)
+
+- (NSString *)mp_imageFingerprint;
+- (NSString *)mp_viewId;
+- (NSString *)mp_controllerVariable;
+
+- (NSString *)mp_varA;
+- (NSString *)mp_varB;
+- (NSString *)mp_varC;
+- (NSArray *)mp_varSetD;
+- (NSString *)mp_varE;
+
+@end
+
 @interface MPCategoryHelpersTests : XCTestCase
 
 @end
@@ -19,14 +33,29 @@
 - (void)testExistence {
     UIView *v1 = [[UIView alloc] init];
     
+    XCTAssert([v1 respondsToSelector:@selector(mp_viewId)]);
+    XCTAssert([v1 respondsToSelector:@selector(mp_controllerVariable)]);
+    XCTAssert([v1 respondsToSelector:@selector(mp_imageFingerprint)]);
+    XCTAssert([v1 respondsToSelector:@selector(mp_imageFingerprint)]);
+    XCTAssert([v1 respondsToSelector:@selector(mp_text)]);
+    
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wundeclared-selector"
+    XCTAssertFalse([v1 respondsToSelector:@selector(mp_nonexistent)]);
+#pragma clang diagnostic pop
+}
+
+- (void)testExistenceLegacy {
+    UIView *v1 = [[UIView alloc] init];
+    
     XCTAssert([v1 respondsToSelector:@selector(mp_varA)]);
     XCTAssert([v1 respondsToSelector:@selector(mp_varB)]);
     XCTAssert([v1 respondsToSelector:@selector(mp_varC)]);
     XCTAssert([v1 respondsToSelector:@selector(mp_varSetD)]);
     XCTAssert([v1 respondsToSelector:@selector(mp_varE)]);
     
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wundeclared-selector"
     XCTAssertFalse([v1 respondsToSelector:@selector(mp_nonexistent)]);
 #pragma clang diagnostic pop
 }
@@ -37,50 +66,61 @@
     UILabel *l1 = [[UILabel alloc] initWithFrame:CGRectMake(1, 2, 3, 4)];
     l1.text = @"label";
     [v1 addSubview:l1];
+    
     UIButton *b1 = [[UIButton alloc] initWithFrame:CGRectMake(2, 3, 4, 5)];
     [b1 setTitle: @"button" forState:UIControlStateNormal];
     [v1 addSubview:b1];
+    
     UIButton *b2 = [[UIButton alloc] initWithFrame:CGRectMake(20, 30, 40, 50)];
     [b2 setTitle: @"button" forState:UIControlStateNormal];
     [v1 addSubview:b2];
     
-    NSString *action = @"signUp";
+    [b1 addTarget:self
+           action:@selector(signUp)
+ forControlEvents:UIControlEventTouchUpInside];
     
-    [b1 addTarget:self action:NSSelectorFromString(action) forControlEvents:UIControlEventTouchUpInside];
-    UIImage *image = [[UIImage alloc] initWithData:[NSData dataWithContentsOfURL:[[NSBundle bundleForClass:[self class]] URLForResource:@"checkerboard" withExtension:@"jpg"]]];
+    UIImage *image = [MPCategoryHelpersTests imageNamed:@"checkerboard"];
     XCTAssert(image);
     [b1 setImage:image forState:UIControlStateNormal];
     
-    // mp_targetActions CONTAINS ...
+    // Legacy
     format = @"mp_varSetD CONTAINS \"ca2e9eeb0bcc438f79c38ffdf5248d8dad4e570c\"";
     XCTAssert([[NSPredicate predicateWithFormat:format] evaluateWithObject:b1]);
     
-    // mp_imageFingerprint
+    // Legacy
     format = @"mp_varC == \"e38b1eed21994d332fc63b581f7fcbe881bfb1f7\"";
     XCTAssert([[NSPredicate predicateWithFormat:format] evaluateWithObject:b1]);
     
+    // Legacy
     format = @"mp_fingerprintVersion == 1 AND mp_varE = \"e0db845a9917b9139c63182c948ac91a5e86b485\"";
     XCTAssert([[NSPredicate predicateWithFormat:format] evaluateWithObject:b1]);
     
     // Predicates with invalid properties
     format = @"mp_nonexistent = 123";
-    XCTAssertThrowsSpecificNamed([[NSPredicate predicateWithFormat:format] evaluateWithObject:b1], NSException, @"NSUnknownKeyException");
+    XCTAssertThrowsSpecificNamed([[NSPredicate predicateWithFormat:format] evaluateWithObject:b1],
+                                 NSException, @"NSUnknownKeyException");
 }
 
 - (void)testImageFingerprint {
     UIButton *b1 = [[UIButton alloc] init];
-    UIImage *image = [[UIImage alloc] initWithData:[NSData dataWithContentsOfURL:[[NSBundle bundleForClass:[self class]] URLForResource:@"huge_checkerboard" withExtension:@"jpg"]]];
+    UIImage *image = [MPCategoryHelpersTests imageNamed:@"huge_checkerboard"];
     XCTAssert(image);
     [b1 setImage:image forState:UIControlStateNormal];
+    
     UIButton *b2 = [[UIButton alloc] init];
-    image = [[UIImage alloc] initWithData:[NSData dataWithContentsOfURL:[[NSBundle bundleForClass:[self class]] URLForResource:@"small_checkerboard" withExtension:@"jpg"]]];
+    image = [MPCategoryHelpersTests imageNamed:@"small_checkerboard"];
     XCTAssert(image);
     [b2 setImage:image forState:UIControlStateNormal];
     
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Wundeclared-selector"
-    XCTAssertEqualObjects([b1 performSelector:@selector(mp_varC)], [b2 performSelector:@selector(mp_varC)]);
-#pragma clang diagnostic pop
+    XCTAssertEqualObjects([b1 mp_imageFingerprint], [b2 mp_imageFingerprint]);
+}
+
++ (UIImage *)imageNamed:(NSString *)name {
+    NSBundle *testBundle = [NSBundle bundleForClass:[self class]];
+    NSURL *URL = [testBundle URLForResource:name
+                              withExtension:@"jpg"];
+    NSData *data = [NSData dataWithContentsOfURL:URL];
+    return [UIImage imageWithData:data];
 }
 
 @end
