73dbf70990ccae624c2c39c7f012f69974b174dd
samgreen <greensamuelm@gmail.com>
2016-06-22 18:17:26 -0700

    3.0.0 Docs - RC7

--- Docs/generate_docs.sh
+++
@@ -1,4 +0,0 @@
-rm -rf ./Mixpanel_h ./MixpanelPeople.h
-headerdoc2html -q -o . ../Mixpanel/MixpanelPeople.h ../Mixpanel/Mixpanel.h
-gatherheaderdoc . index.html
-open index.html
--- Mixpanel/MPFoundation.h
+++
@@ -1,8 +0,0 @@
-#import <Foundation/Foundation.h>
-
-#if TARGET_OS_IPHONE
-#ifndef NSFoundationVersionNumber_iOS_8_0
-// support for Xcode 6.*
-#define NSFoundationVersionNumber_iOS_8_0 1140.11
-#endif
-#endif
