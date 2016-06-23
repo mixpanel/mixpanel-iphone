376cdf3ab0c3a6a04126082e6f545c3ac62450c1
samgreen <greensamuelm@gmail.com>
2016-06-23 10:15:04 -0700

    Revert docs generation

--- Docs/generate_docs.sh
+++ Docs/generate_docs.sh
@@ -1,4 +1,6 @@
-rm -rf ./Mixpanel_h ./MixpanelPeople.h
-headerdoc2html -q -o . ../Mixpanel/MixpanelPeople.h ../Mixpanel/Mixpanel.h
-gatherheaderdoc . index.html
-open index.html
+rm -rf ./Mixpanel_h
+rm Mixpanel.h
+cat ../Mixpanel/MixpanelPeople.h ../Mixpanel/Mixpanel.h >> Mixpanel.h
+headerdoc2html -q -o . Mixpanel.h
+rm Mixpanel.h
+open Mixpanel_h/index.html
