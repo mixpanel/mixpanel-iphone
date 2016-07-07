9945000ff52cdbc01da175f2aac220a645bae9a9
Antoine Cœur <coeur@gmx.fr>
2016-07-07 09:31:12 +0800

    Update MPFoundation.h
    Droping support for Xcode 6, adding support for Xcode 7.

--- Mixpanel/MPFoundation.h
+++ Mixpanel/MPFoundation.h
@@ -1,8 +1,10 @@
 #import <Foundation/Foundation.h>
 
 #if TARGET_OS_IPHONE
-#ifndef NSFoundationVersionNumber_iOS_8_0
-// support for Xcode 6.*
-#define NSFoundationVersionNumber_iOS_8_0 1140.11
+#ifndef NSFoundationVersionNumber_iOS_9_0
+// support for Xcode 7.*
+#define NSFoundationVersionNumber_iOS_8_x_Max 1199
+#define NSFoundationVersionNumber_iOS_9_0 1240.1
+#define NSFoundationVersionNumber_iOS_9_x_Max 1299
 #endif
 #endif
