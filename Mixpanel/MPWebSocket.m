6140e4e036456fc8d950f3b5955bbb36301e4e39
Mike Welsh <mike@ritual.co>
2016-07-18 09:48:36 -0400

    Add in logging to check the result of SecRandomCopyBytes

--- Mixpanel/MPWebSocket.m
+++ Mixpanel/MPWebSocket.m
@@ -520,7 +520,10 @@ static __strong NSData *CRLFCRLF;
     CFHTTPMessageSetHeaderFieldValue(request, CFSTR("Host"), (__bridge CFStringRef)(_url.port ? [NSString stringWithFormat:@"%@:%@", _url.host, _url.port] : _url.host));
 
     NSMutableData *keyBytes = [[NSMutableData alloc] initWithLength:16];
-    SecRandomCopyBytes(kSecRandomDefault, keyBytes.length, keyBytes.mutableBytes);
+    int result = SecRandomCopyBytes(kSecRandomDefault, keyBytes.length, keyBytes.mutableBytes);
+    if (result != 0) {
+        MPLogWarning(@"SecRandomCopyBytes failed with: %d", result);
+    }
     _secKey = [keyBytes base64EncodedStringWithOptions:NSDataBase64Encoding64CharacterLineLength];
     assert(_secKey.length == 24);
 
@@ -1389,7 +1392,10 @@ static const size_t MPFrameHeaderOverhead = 32;
         }
     } else {
         uint8_t *mask_key = frame_buffer + frame_buffer_size;
-        SecRandomCopyBytes(kSecRandomDefault, sizeof(uint32_t), mask_key);
+        int result = SecRandomCopyBytes(kSecRandomDefault, sizeof(uint32_t), mask_key);
+        if (result != 0) {
+            MPLogWarning(@"SecRandomCopyBytes failed with: %d", result);
+        }
         frame_buffer_size += sizeof(uint32_t);
 
         // TODO: could probably optimize this with SIMD
