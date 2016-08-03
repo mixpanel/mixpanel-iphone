7f54f0e33371cdc2c83a99c7f8308acc7e169092
Yarden Eitan <yarneo@gmail.com>
2016-08-03 14:40:33 -0400

    updated docs script and bumped version to v3.0.1

---
+++ generate_docs.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "Generating documentation."
+
+rm -rf ./Docs/
+headerdoc2html -q -o ./Docs ./Mixpanel/Mixpanel.h > /dev/null 2>&1
+rm -rf ../TempMixpanelDocs
+mkdir ../TempMixpanelDocs
+cp -a ./Docs/Mixpanel_h/. ../TempMixpanelDocs/.
+rm -rf ./Docs/
+git checkout gh-pages
+git pull
+cp -a ../TempMixpanelDocs/. .
+rm -rf ../TempMixpanelDocs/
+git commit -am "updated docs"
+git push origin gh-pages
+git checkout master
+
+echo "Updated docs and pushed them to gh-pages!"
