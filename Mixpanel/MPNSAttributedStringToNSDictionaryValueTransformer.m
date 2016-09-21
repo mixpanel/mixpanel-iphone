70b6ab0be5763ced6a12c6d1431570afd4dbeb0c
Yarden Eitan <yarneo@gmail.com>
2016-09-21 16:28:50 -0700

    fix for issue #478

--- Mixpanel/MPNSAttributedStringToNSDictionaryValueTransformer.m
+++ Mixpanel/MPNSAttributedStringToNSDictionaryValueTransformer.m
@@ -19,7 +19,21 @@
 - (id)transformedValue:(id)value
 {
     if ([value isKindOfClass:[NSAttributedString class]]) {
-        NSAttributedString *attributedString = value;
+        NSMutableAttributedString *attributedString = [value mutableCopy];
+        [attributedString beginEditing];
+        __block BOOL safe = NO;
+        [attributedString enumerateAttribute:NSParagraphStyleAttributeName inRange:NSMakeRange(0, attributedString.length) options:0 usingBlock:^(id value, NSRange range, BOOL *stop) {
+            if (value) {
+                NSParagraphStyle *paragraphStyle = value;
+                if([paragraphStyle respondsToSelector:@selector(headIndent)]) {
+                    safe = YES;
+                }
+            }
+        }];
+        if (!safe) {
+            [attributedString removeAttribute:NSParagraphStyleAttributeName range:NSMakeRange(0, attributedString.length)];
+        }
+        [attributedString endEditing];
 
         NSError *error = nil;
         NSData *data = [attributedString dataFromRange:NSMakeRange(0, attributedString.length)
