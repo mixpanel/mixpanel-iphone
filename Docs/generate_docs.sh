67a15adc7ce60ee0eb5a198b4fbf454972f79a77
Yarden Eitan <yarneo@gmail.com>
2017-03-30 14:37:23 -0700

    updated docs

--- Docs/generate_docs.sh
+++
@@ -1,6 +0,0 @@
-rm -rf ./Mixpanel_h
-rm Mixpanel.h
-cat ../Mixpanel/MixpanelPeople.h ../Mixpanel/Mixpanel.h >> Mixpanel.h
-headerdoc2html -q -o . Mixpanel.h
-rm Mixpanel.h
-open Mixpanel_h/index.html
