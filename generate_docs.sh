69c2c6e63b7eb7cad157ab17f904ba9d9df781bc
Peter Chien <peter.chien@mixpanel.com>
2017-09-14 15:56:34 -0700

    update generate docs script to use appledoc

--- generate_docs.sh
+++ generate_docs.sh
@@ -2,17 +2,18 @@
 
 echo "Generating documentation."
 
-rm -rf ./Docs/
-headerdoc2html -q -o ./Docs ./Mixpanel/Mixpanel.h ./Mixpanel/MixpanelPeople.h > /dev/null 2>&1
+rm -rf ./Docs
+appledoc --project-name mixpanel-iphone --project-company Mixpanel --company-id com.mixpanel --no-repeat-first-par --keep-undocumented-objects --keep-undocumented-members --preprocess-headerdoc --no-create-docset --output Docs Mixpanel/Mixpanel.h Mixpanel/MixpanelPeople.h
 rm -rf ../TempMixpanelDocs
-mkdir ../TempMixpanelDocs
-cp -a ./Docs/Mixpanel_h/. ../TempMixpanelDocs/.
-rm -rf ./Docs/
+cp -a ./Docs/html/. ../TempMixpanelDocs
+rm -rf ./Docs
 git checkout gh-pages
 git pull
 cp -a ../TempMixpanelDocs/. .
 rm -rf ../TempMixpanelDocs/
-git commit -am "updated docs"
+git add .
+git reset HelloMixpanel/
+git commit -m 'updated docs'
 git push origin gh-pages
 git checkout master
 
