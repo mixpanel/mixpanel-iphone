c0073690f5e1492831a27691c484650188026a0c
Will Ginsberg <willginsberg@gmail.com>
2017-12-18 16:19:41 -0800

    only attempt to convert and add push token if present (#756)
    * only convert token to string if token present
    
    * convert token and then check for non nil value

--- Mixpanel/MixpanelPeople.m
+++ Mixpanel/MixpanelPeople.m
@@ -150,8 +150,11 @@
 
 - (void)addPushDeviceToken:(NSData *)deviceToken
 {
-    NSDictionary *properties = @{@"$ios_devices": @[[MixpanelPeople pushDeviceTokenToString:deviceToken]]};
-    [self addPeopleRecordToQueueWithAction:@"$union" andProperties:properties];
+    NSString *tokenString = [MixpanelPeople pushDeviceTokenToString:deviceToken]
+    if(tokenString){
+        NSDictionary *properties = @{@"$ios_devices": @[tokenString]};
+        [self addPeopleRecordToQueueWithAction:@"$union" andProperties:properties];
+    }
 }
 
 - (void)removeAllPushDeviceTokens
