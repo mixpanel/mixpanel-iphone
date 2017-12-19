6a24a56578a761b64ecce95119a046c7285e1cd0
Will Ginsberg <willginsberg@gmail.com>
2017-12-18 16:17:23 -0800

    convert token and then check for non nil value

--- Mixpanel/MixpanelPeople.m
+++ Mixpanel/MixpanelPeople.m
@@ -150,8 +150,9 @@
 
 - (void)addPushDeviceToken:(NSData *)deviceToken
 {
-    if (deviceToken){
-        NSDictionary *properties = @{@"$ios_devices": @[[MixpanelPeople pushDeviceTokenToString:deviceToken]]};
+    NSString *tokenString = [MixpanelPeople pushDeviceTokenToString:deviceToken]
+    if(tokenString){
+        NSDictionary *properties = @{@"$ios_devices": @[tokenString]};
         [self addPeopleRecordToQueueWithAction:@"$union" andProperties:properties];
     }
 }
