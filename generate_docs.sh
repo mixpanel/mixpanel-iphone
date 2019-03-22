25c9814dd5d39ce9dedb22fbed4fd872aabe5e52
Zihe Jia <zihe.jia@mixpanel.com>
2019-03-22 14:26:40 -0700

    Update generate docs script to include Group Class

--- generate_docs.sh
+++ generate_docs.sh
@@ -3,7 +3,7 @@
 echo "Generating documentation."
 
 rm -rf docs
-appledoc --project-name mixpanel-iphone --project-company Mixpanel --company-id com.mixpanel --no-repeat-first-par --keep-undocumented-objects --keep-undocumented-members --preprocess-headerdoc --no-create-docset --output docs Mixpanel/Mixpanel.h Mixpanel/MixpanelPeople.h
+appledoc --project-name mixpanel-iphone --project-company Mixpanel --company-id com.mixpanel --no-repeat-first-par --keep-undocumented-objects --keep-undocumented-members --preprocess-headerdoc --no-create-docset --output docs Mixpanel/Mixpanel.h Mixpanel/MixpanelPeople.h Mixpanel/MixpanelGroup.h
 cp -a docs/html/. docs/.
 rm -rf docs/html
 git add docs
