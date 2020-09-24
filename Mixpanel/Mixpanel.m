35a26e97fbf28fe72177ce8710c47051fadc49bf
Zihe Jia <zihe.jia@mixpanel.com>
2020-09-24 16:52:39 -0700

    Version 3.6.3

--- Mixpanel/Mixpanel.m
+++ Mixpanel/Mixpanel.m
@@ -31,7 +31,7 @@
 #error The Mixpanel library must be compiled with ARC enabled
 #endif
 
-#define VERSION @"3.6.2"
+#define VERSION @"3.6.3"
 
 NSString *const MPNotificationTypeMini = @"mini";
 NSString *const MPNotificationTypeTakeover = @"takeover";
