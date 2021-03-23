310faa3f236946a951eb65a38640c294271f4832
Zihe Jia <zihe.jia@mixpanel.com>
2021-03-23 15:53:38 -0700

    add swift package manager

---
+++ Package.swift
@@ -0,0 +1,28 @@
+// swift-tools-version:5.3
+
+import PackageDescription
+
+let package = Package(
+    name: "Mixpanel",
+    platforms: [
+      .iOS(.v9),
+      .tvOS(.v9),
+      .macOS(.v10_10),
+      .watchOS(.v3),
+    ],
+    products: [
+        .library(name: "Mixpanel", targets: ["Mixpanel"]),
+    ],
+    targets: [
+        .target(
+            name: "Mixpanel",
+            path: "Sources",
+            exclude: [
+                "Info.plist"
+            ],
+            cSettings: [
+                .headerSearchPath("."),
+            ]
+        ),
+    ]
+)
