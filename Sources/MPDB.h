07576ac17f69ee41ea72746c2f35acf631ebba3d
Jared McFarland <jared@mixpanel.com>
2021-09-20 11:26:55 -0700

    port mpdb

---
+++ Sources/MPDB.h
@@ -0,0 +1,33 @@
+//
+//  MPDB.h
+//  Mixpanel
+//
+//  Created by Jared McFarland on 9/17/21.
+//  Copyright © 2021 Mixpanel. All rights reserved.
+//
+
+#ifndef MPDB_h
+#define MPDB_h
+
+#import <Foundation/Foundation.h>
+#import <sqlite3.h>
+
+@interface MPDB : NSObject {
+    NSString *_apiToken;
+    sqlite3 *_connection;
+}
+
+@property (nonatomic, copy) NSString *apiToken;
+
+- (void) open;
+- (void) close;
+
+- (void) insertRow:(NSString *)persistenceType data:(NSData *)data flag:(bool) flag;
+- (void) deleteRows:(NSString *)persistenceType ids:(NSArray *)ids;
+- (void) updateRowsFlag:(NSString *)persistenceType newFlag:(bool)newFlag;
+- (NSArray *) readRows:(NSString *)persistenceType numRows:(int)numRows flag:(bool)flag;
+
+@end
+
+
+#endif /* MPDB_h */
