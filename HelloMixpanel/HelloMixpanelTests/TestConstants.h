3b1256b056e4295332f09ecac47d2b1ab6e2010c
Zihe Jia <zihe.jia@mixpanel.com>
2022-01-31 14:26:16 -0800

    remove nocilla and ocmock

--- HelloMixpanel/HelloMixpanelTests/TestConstants.h
+++ HelloMixpanel/HelloMixpanelTests/TestConstants.h
@@ -5,26 +5,8 @@
 //  Copyright © Mixpanel. All rights reserved.
 //
 
-#import <Nocilla/Nocilla.h>
 
 #pragma mark - Constants
-static NSString *const kTestToken = @"abc123";
+static NSString *const kFakeServerUrl = @"https://34a272abf23d.com";
+
 static NSString *const kDefaultServerString = @"https://api.mixpanel.com";
-
-#pragma mark - Stub Helpers
-static inline LSStubRequestDSL *stubEngage() {
-    return stubRequest(@"POST", @"https://api.mixpanel.com/engage/".regex).withHeader(@"Content-Type", @"application/json");
-}
-
-static inline LSStubRequestDSL *stubTrack() {
-    return stubRequest(@"POST", @"https://api.mixpanel.com/track/".regex).withHeader(@"Content-Type", @"application/json");
-}
-
-static inline LSStubRequestDSL *stubGroups() {
-    return stubRequest(@"POST", @"https://api.mixpanel.com/groups/".regex).withHeader(@"Content-Type", @"application/json");
-}
-
-static inline LSStubRequestDSL *stubDecide() {
-    return stubRequest(@"POST", @"https://api.mixpanel.com/decide(.*?)".regex).withHeader(@"Content-Type", @"application/json");
-}
-
